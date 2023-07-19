625d39a852677ac5349814abe5dbfc52f6a35174
Onur Dabanca <onurdbnc@gmail.com>
2023-07-20 02:25:40 +0300

    Add project files.

---
+++ Security.WebUI/Models/CustomProduct.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Security.WebUI.Models
+{
+    public partial class Product
+    {
+        [NotMapped]
+        public string EncrypedId { get; set; }
+    }
+}
---
+++ Security.WebUI/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Security.WebUI.Models;
+
+namespace Security.WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddDataProtection();
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<NorthwindContext>(opts =>
+            {
+                opts.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
