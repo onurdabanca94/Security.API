f689eb58d32f4ef2831adabd45b065313fa5a419
Onur Dabanca <onurdbnc@gmail.com>
2023-07-20 04:39:59 +0300

    SecretProtection Project includes ConnectionString's value will hidden above the secrets.json

---
+++ SecretProtection.WebUI/Program.cs
@@ -0,0 +1,43 @@
+using System.Data.SqlClient;
+
+namespace SecretProtection.WebUI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            string connectionString = builder.Configuration["ConnectionStrings:SqlConnectionString"];
+            //var options = new SqlConnectionStringBuilder(connectionString);
+            //options.Password = builder.Configuration["Passwords:SqlPassword"];
+            //string connString = options.ConnectionString;
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
